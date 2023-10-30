07c5f66bd5f547b6eae38a72b08f662670e96473
kouki <koukikarim507@gmail.com>
2023-10-30 15:37:40 +0100

    prosit5

---
+++ src/terrestrial.java
@@ -0,0 +1,16 @@
+
+public  class terrestrial extends Animal {
+
+    private int nbrLegs;
+
+
+    public terrestrial(String family, String name, int age, boolean isMammal, int nbrLegs) {
+        super(family, name, age, isMammal);
+        this.nbrLegs = nbrLegs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", nbrLegs:" + nbrLegs;
+    }
+}
